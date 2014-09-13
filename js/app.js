4fa242036242f18b19c7459222b7d6c57d67776d
Jesse Showalter <iamjesseshowalter@gmail.com>
2014-09-12 21:29:54 -1000

    bulk upload

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+	//  main button click function
+	$('button#create-task').on('click', function(){
+
+		// remove nothing message
+		if ('.nothing-message') {
+			// $('.nothing-message').fadeOut('fast');
+			$('.nothing-message').hide('slide',{direction:'left'},300)
+		};
+
+		// create the new li from the form input
+		var task = $('input[name=task-insert]').val();
+		var newTask = '<li>' + '<p>'+task+'</p>' + '</li>'
+		$('#task-list').append(newTask);
+
+		// clear form when button is pressed
+		$('input').val('');
+
+		// Alert if the form in submitted empty
+		if (task.length == 0) {
+			alert('please enter a task');
+		};
+
+		// makes other controls fade in when first task is created
+		$('#controls').fadeIn();
+		$('.task-headline').fadeIn();
+	});
+
+	// use enter to add list items
+	$('input').keyup(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('button').click();
+		};
+	});
+
+	// mark as complete
+	$(document).on('click','li',function(){
+		$(this).toggleClass('complete');
+	});
+	
+	// double click to remove
+	$(document).on('dblclick','li',function(){
+		$(this).fadeOut('fast');
+	});
+
+	// Clear all tasks button
+	$('button#clear-all-tasks').on('click', function(){
+		$('#task-list li').remove();
+		$('.task-headline').fadeOut();
+		$('#controls').fadeOut();
+		$('.nothing-message').show('fast');
+	});
+});
